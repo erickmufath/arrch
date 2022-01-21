59916b98d8493bda010d2527421cdde8239160a1
erickmufath <90117993+erickmufath@users.noreply.github.com>
2022-01-22 00:48:12 +0700

    Update 1-post-setup.sh

--- 1-post-setup.sh
+++ 1-post-setup.sh
@@ -12,15 +12,11 @@ lsblk
 echo "======================================================="
 echo ""
 read -p "->] Input Drive untuk install Bootloader (Contoh : sda, sdb, atau nvmen1) = " drive
-pacman -Sy sed archlinux-keyring pacman --noconfirm --needed
 #Add parallel downloading
 #sed -i 's/^#Para/Para/' /etc/pacman.conf
 sed -i 's/^#ParallelDownloads = 5/ParallelDownloads = 2/' /etc/pacman.conf
 #Enable multilib
 sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
-rm -r /etc/pacman.d/gnupg
-pacman-key --init
-pacman-key --populate archlinux
 #basestrap
 pacstrap /mnt --needed - < arrch/_base.txt
 pacstrap /mnt --needed - < arrch/_base.txt
